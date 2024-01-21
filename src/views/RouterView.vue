b88914e4aa71ffc9331c3774c1e60e2dec13b34d
Dieg0CLima <diego.lima@agetelecom.com.br>
2024-01-20 22:04:48 -0300

    sei la

--- src/views/RouterView.vue
+++ src/views/RouterView.vue
@@ -8,276 +8,31 @@ const contract = ref(null);
 const error = ref(null);
 
 const fetchContract = async () => {
+  if (!searchTerm.value) {
+    error.value = "Por favor, insira um número de contrato.";
+    return;
+  }
+
   try {
-    const response = await axios.get('http://localhost:3000/api/contracts/69352');
+    const response = await axios.get(`http://localhost:3000/api/contracts/${searchTerm.value}`);
     contract.value = response.data;
+    error.value = null;
   } catch (e) {
     error.value = e.message;
   }
 };
 
+
 onMounted(fetchContract);
 
-
-const sampleData = [
-  {
-    Contrato: "C12345",
-    Cliente: "Empresa XYZ",
-    Status: "Contrato: Ativo",
-    Estagio: "Contrato: Em andamento",
-    IniciodoContrato: "2023-01-15",
-    Telefone1: "11987654321",
-    Telefone2: "11987654322",
-    CPF: "123.456.789-00",
-    Email: "cliente@empresaxyz.com",
-    Endereco: "Rua Exemplo, 123",
-    Cidade: "São Paulo",
-    CEP: "12345-678",
-    Plano: "Fibra Ótica 100MB",
-    OLT: "OLT-Modelo123",
-    Usuario: "usuarioXYZ",
-    Senha: "senha123",
-    IdONU: "ONU12345678",
-    Slot: "5",
-    Port: "10",
-    Splitter: "Splitter-ModeloA",
-  },
-  {
-    Contrato: "C12345",
-    Cliente: "Empresa XYZ",
-    Status: "Contrato: Ativo",
-    Estagio: "Contrato: Em andamento",
-    IniciodoContrato: "2023-01-15",
-    Telefone1: "11987654321",
-    Telefone2: "11987654322",
-    CPF: "123.456.789-00",
-    Email: "cliente@empresaxyz.com",
-    Endereco: "Rua Exemplo, 123",
-    Cidade: "São Paulo",
-    CEP: "12345-678",
-    Plano: "Fibra Ótica 100MB",
-    OLT: "OLT-Modelo123",
-    Usuario: "usuarioXYZ",
-    Senha: "senha123",
-    IdONU: "ONU12345678",
-    Slot: "5",
-    Port: "10",
-    Splitter: "Splitter-ModeloA",
-  },
-  {
-    Contrato: "C12345",
-    Cliente: "Empresa XYZ",
-    Status: "Contrato: Ativo",
-    Estagio: "Contrato: Em andamento",
-    IniciodoContrato: "2023-01-15",
-    Telefone1: "11987654321",
-    Telefone2: "11987654322",
-    CPF: "123.456.789-00",
-    Email: "cliente@empresaxyz.com",
-    Endereco: "Rua Exemplo, 123",
-    Cidade: "São Paulo",
-    CEP: "12345-678",
-    Plano: "Fibra Ótica 100MB",
-    OLT: "OLT-Modelo123",
-    Usuario: "usuarioXYZ",
-    Senha: "senha123",
-    IdONU: "ONU12345678",
-    Slot: "5",
-    Port: "10",
-    Splitter: "Splitter-ModeloA",
-  },
-  {
-    Contrato: "C12345",
-    Cliente: "Empresa XYZ",
-    Status: "Contrato: Ativo",
-    Estagio: "Contrato: Em andamento",
-    IniciodoContrato: "2023-01-15",
-    Telefone1: "11987654321",
-    Telefone2: "11987654322",
-    CPF: "123.456.789-00",
-    Email: "cliente@empresaxyz.com",
-    Endereco: "Rua Exemplo, 123",
-    Cidade: "São Paulo",
-    CEP: "12345-678",
-    Plano: "Fibra Ótica 100MB",
-    OLT: "OLT-Modelo123",
-    Usuario: "usuarioXYZ",
-    Senha: "senha123",
-    IdONU: "ONU12345678",
-    Slot: "5",
-    Port: "10",
-    Splitter: "Splitter-ModeloA",
-  },
-  {
-    Contrato: "C12345",
-    Cliente: "Empresa XYZ",
-    Status: "Contrato: Ativo",
-    Estagio: "Contrato: Em andamento",
-    IniciodoContrato: "2023-01-15",
-    Telefone1: "11987654321",
-    Telefone2: "11987654322",
-    CPF: "123.456.789-00",
-    Email: "cliente@empresaxyz.com",
-    Endereco: "Rua Exemplo, 123",
-    Cidade: "São Paulo",
-    CEP: "12345-678",
-    Plano: "Fibra Ótica 100MB",
-    OLT: "OLT-Modelo123",
-    Usuario: "usuarioXYZ",
-    Senha: "senha123",
-    IdONU: "ONU12345678",
-    Slot: "5",
-    Port: "10",
-    Splitter: "Splitter-ModeloA",
-  },
-  {
-    Contrato: "C12345",
-    Cliente: "Empresa XYZ",
-    Status: "Contrato: Ativo",
-    Estagio: "Contrato: Em andamento",
-    IniciodoContrato: "2023-01-15",
-    Telefone1: "11987654321",
-    Telefone2: "11987654322",
-    CPF: "123.456.789-00",
-    Email: "cliente@empresaxyz.com",
-    Endereco: "Rua Exemplo, 123",
-    Cidade: "São Paulo",
-    CEP: "12345-678",
-    Plano: "Fibra Ótica 100MB",
-    OLT: "OLT-Modelo123",
-    Usuario: "usuarioXYZ",
-    Senha: "senha123",
-    IdONU: "ONU12345678",
-    Slot: "5",
-    Port: "10",
-    Splitter: "Splitter-ModeloA",
-  },
-  {
-    Contrato: "C12345",
-    Cliente: "Empresa XYZ",
-    Status: "Contrato: Ativo",
-    Estagio: "Contrato: Em andamento",
-    IniciodoContrato: "2023-01-15",
-    Telefone1: "11987654321",
-    Telefone2: "11987654322",
-    CPF: "123.456.789-00",
-    Email: "cliente@empresaxyz.com",
-    Endereco: "Rua Exemplo, 123",
-    Cidade: "São Paulo",
-    CEP: "12345-678",
-    Plano: "Fibra Ótica 100MB",
-    OLT: "OLT-Modelo123",
-    Usuario: "usuarioXYZ",
-    Senha: "senha123",
-    IdONU: "ONU12345678",
-    Slot: "5",
-    Port: "10",
-    Splitter: "Splitter-ModeloA",
-  },
-  {
-    Contrato: "C12345",
-    Cliente: "Empresa XYZ",
-    Status: "Contrato: Ativo",
-    Estagio: "Contrato: Em andamento",
-    IniciodoContrato: "2023-01-15",
-    Telefone1: "11987654321",
-    Telefone2: "11987654322",
-    CPF: "123.456.789-00",
-    Email: "cliente@empresaxyz.com",
-    Endereco: "Rua Exemplo, 123",
-    Cidade: "São Paulo",
-    CEP: "12345-678",
-    Plano: "Fibra Ótica 100MB",
-    OLT: "OLT-Modelo123",
-    Usuario: "usuarioXYZ",
-    Senha: "senha123",
-    IdONU: "ONU12345678",
-    Slot: "5",
-    Port: "10",
-    Splitter: "Splitter-ModeloA",
-  },
-  {
-    Contrato: "C12345",
-    Cliente: "Empresa XYZ",
-    Status: "Contrato: Ativo",
-    Estagio: "Contrato: Em andamento",
-    IniciodoContrato: "2023-01-15",
-    Telefone1: "11987654321",
-    Telefone2: "11987654322",
-    CPF: "123.456.789-00",
-    Email: "cliente@empresaxyz.com",
-    Endereco: "Rua Exemplo, 123",
-    Cidade: "São Paulo",
-    CEP: "12345-678",
-    Plano: "Fibra Ótica 100MB",
-    OLT: "OLT-Modelo123",
-    Usuario: "usuarioXYZ",
-    Senha: "senha123",
-    IdONU: "ONU12345678",
-    Slot: "5",
-    Port: "10",
-    Splitter: "Splitter-ModeloA",
-  },
-  {
-    Contrato: "C12345",
-    Cliente: "Empresa XYZ",
-    Status: "Contrato: Ativo",
-    Estagio: "Contrato: Em andamento",
-    IniciodoContrato: "2023-01-15",
-    Telefone1: "11987654321",
-    Telefone2: "11987654322",
-    CPF: "123.456.789-00",
-    Email: "cliente@empresaxyz.com",
-    Endereco: "Rua Exemplo, 123",
-    Cidade: "São Paulo",
-    CEP: "12345-678",
-    Plano: "Fibra Ótica 100MB",
-    OLT: "OLT-Modelo123",
-    Usuario: "usuarioXYZ",
-    Senha: "senha123",
-    IdONU: "ONU12345678",
-    Slot: "5",
-    Port: "10",
-    Splitter: "Splitter-ModeloA",
-  },
-  {
-    Contrato: "C12345",
-    Cliente: "Empresa XYZ",
-    Status: "Contrato: Ativo",
-    Estagio: "Contrato: Em andamento",
-    IniciodoContrato: "2023-01-15",
-    Telefone1: "11987654321",
-    Telefone2: "11987654322",
-    CPF: "123.456.789-00",
-    Email: "cliente@empresaxyz.com",
-    Endereco: "Rua Exemplo, 123",
-    Cidade: "São Paulo",
-    CEP: "12345-678",
-    Plano: "Fibra Ótica 100MB",
-    OLT: "OLT-Modelo123",
-    Usuario: "usuarioXYZ",
-    Senha: "senha123",
-    IdONU: "ONU12345678",
-    Slot: "5",
-    Port: "10",
-    Splitter: "Splitter-ModeloA",
-  },
-];
 </script>
 <template>
   <div class="page-container">
     <div class="btn-back">
       <router-link to="/">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="16"
-          width="16"
-          viewBox="0 0 512 512"
-        >
-          <path
-            fill="#ffffff"
-            d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 288 480 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-370.7 0 73.4-73.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-128 128z"
-          />
+        <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
+          <path fill="#ffffff"
+            d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 288 480 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-370.7 0 73.4-73.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-128 128z" />
         </svg>
       </router-link>
     </div>
@@ -285,9 +40,7 @@ const sampleData = [
       <label>Pesquisar</label>
     </div>
     <div class="subtitle-page">
-      <label
-        >Experimente realizar a consulta com Nome, CPF, Telefone, Contrato ou ONU</label
-      >
+      <label>Experimente realizar a consulta com Nome, CPF, Telefone, Contrato ou ONU</label>
     </div>
     <div class="search-container">
       <input type="text" placeholder="Digite aqui..." v-model="searchTerm" />
@@ -299,21 +52,13 @@ const sampleData = [
           <option value="contrato">Contrato</option>
           <option value="onu">ONU</option>
         </select>
-        <div class="btn-search" @click="search">
+        <div class="btn-search" @click="fetchContract">
           <label>Pesquisar</label>
-          <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="16"
-              width="16"
-              viewBox="0 0 512 512"
-          >
-            <path
-                fill="#ffffff"
-                d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6
+          <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
+            <path fill="#ffffff" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6
           457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4
           25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416
-          208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"
-            />
+          208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
           </svg>
         </div>
       </div>
@@ -321,21 +66,14 @@ const sampleData = [
 
     <div class="grid-container">
       <div class="contract-card" v-for="item in sampleData" :key="item.Contrato">
-        <h3>{{ item.Contrato }}</h3>
-        <div class="contract-details">
-          <p>{{ item.IdONU }}</p>
-          <p>{{ item.CPF }}</p>
-          <p>{{ item.Cliente }}</p>
-          <p v-if="contract">ID do Contrato: {{ contract.id }}</p>
-          <p v-if="error">{{ error }}</p>
-        </div>
+        <p>ID do Contrato: {{ contract.contractNumber }}</p>
+        <div v-if="error">{{ error }}</div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-
 .page-container {
   width: 100vw;
   height: 100vh;
